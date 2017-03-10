4d67a840ab7abf86eb4239f4036c7c33a24c4979
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2017-03-11 01:00:12 +0900

    add setup configuration

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# setup the configuration files
+
+
+paths=("fish" "nvim" "pt" "ranger")
+
+for mypath in ${paths[@]}; do
+    if [ ! -d ~/.config/$mypath  ]; then
+        ln -s $PWD/$mypath ~/.config/$mypath
+        echo "[OK] linked '~/.config/$mypath'"
+    else
+        echo "[NG] '~/.config/$mypath' exist"
+    fi
+done
+
+paths=("tigrc" "inputrc")
+
+for mypath in ${paths[@]}; do
+    if [ ! -e ~/.$mypath  ]; then
+        ln -s $PWD/$mypath/.$mypath ~/.$mypath
+        echo "[OK] linked '~/.$mypath'"
+    else
+        echo "[NG] '~/.$mypath' exist"
+    fi
+done
