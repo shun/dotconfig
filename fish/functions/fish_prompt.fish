e8cf91772588457bf67ec83e19f48bca04418fae
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2017-03-10 23:53:14 +0900

    set prompt color

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,14 @@
 function fish_prompt
     set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
     echo -e ''
-    echo -n (whoami)'@'(hostname)':'(prompt_pwd)' {'"$git_branch"'}'
+    set_color 708090
+    echo -n (whoami)'@'
+    set_color -o yellow
+    echo -n (hostname)
+    set_color normal
+    set_color 708090
+    echo -n ':'(prompt_pwd)' {'"$git_branch"'}'
+    set_color normal
     echo -e '\n$ '
 end
 
